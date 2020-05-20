b4828e8da3bade5c6d547899a2547a32adc22d32
daniel <daniel.mullis@gmail.com>
2020-05-20 04:52:05 -0400

    commandline runner and logging support

--- src/main/java/com/ieinadex/jira/releasenotes/generator/Application.java
+++ src/main/java/com/ieinadex/jira/releasenotes/generator/Application.java
@@ -1,13 +1,30 @@
 package com.ieinadex.jira.releasenotes.generator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        try {
+            logger.info("Project started");
+
+        }
+        catch (Exception ex) {
+            logger.error("General error starting application: {}", ex.getMessage());
+        }
+
+    }
 }
