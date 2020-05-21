3b9fc3bf2d4bbe2c8a42df19e78656287be2aadc
daniel <daniel.mullis@gmail.com>
2020-05-20 23:09:28 -0400

    commandline runner and logging support

--- src/main/java/com/ieinadex/jira/releasenotes/generator/Application.java
+++ src/main/java/com/ieinadex/jira/releasenotes/generator/Application.java
@@ -2,12 +2,19 @@ package com.ieinadex.jira.releasenotes.generator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
+
+    @Value("${com.ieinadex.jira.releasenotes.generator.jiraBaseUrl}")
+    private String jiraBaseUrl;
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -20,6 +27,13 @@ public class Application implements CommandLineRunner {
 
         try {
             logger.info("Project started");
+            WebClient client = WebClient.builder()
+                    .baseUrl(jiraBaseUrl)
+                    .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .build();
+
+            logger.info("base URL: {}", jiraBaseUrl);
+
 
         }
         catch (Exception ex) {
