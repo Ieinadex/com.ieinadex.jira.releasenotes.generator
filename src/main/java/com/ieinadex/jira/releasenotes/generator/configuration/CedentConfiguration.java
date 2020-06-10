d9acf49e49a20ffb1b2001173f774ca37fc2da04
daniel <daniel.mullis@gmail.com>
2020-06-10 04:05:29 -0400

    cleanup

--- src/main/java/com/ieinadex/jira/releasenotes/generator/configuration/CedentConfiguration.java
+++ src/main/java/com/ieinadex/jira/releasenotes/generator/configuration/CedentConfiguration.java
@@ -1,19 +1,11 @@
 package com.ieinadex.jira.releasenotes.generator.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Component
 @ConfigurationProperties("cedent")
